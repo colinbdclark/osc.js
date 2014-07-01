b944ab9bee34bc70b24623ab0e403ffba20c5e30
xseignard <xavier.seignard@gmail.com>
2014-07-01 13:50:16 +0200

    Simpler way to convert UInt8Array to ArrayBuffer

--- src/platforms/osc-chromeapp.js
+++ src/platforms/osc-chromeapp.js
@@ -139,11 +139,7 @@ var osc = osc || {};
         address = address || o.remoteAddress;
         port = port !== undefined ? port : o.remotePort;
 
-        var buffer = new ArrayBuffer(encoded.length);
-        var bufferView = new Uint8Array(buffer);
-        bufferView.set(encoded);
-
-        chrome.sockets.udp.send(this.socketId, buffer, address, port, function (info) {
+        chrome.sockets.udp.send(this.socketId, encoded.buffer, address, port, function (info) {
             if (!info) {
                 that.emit("error",
                     "There was an unknown error while trying to send a UDP message. " +

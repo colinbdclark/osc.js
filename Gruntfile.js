fea9389ba675be854cbb099e8c4dae843d827d42
Colin Clark <colinbdclark@gmail.com>
2015-08-02 21:18:46 -0400

    Deduplicates infusion when npm installing.

--- Gruntfile.js
+++ Gruntfile.js
@@ -118,6 +118,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-contrib-uglify");
     grunt.loadNpmTasks("grunt-contrib-clean");
     grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-gpii");
 
     grunt.registerTask("default", ["clean", "jshint", "concat", "uglify"]);
 };

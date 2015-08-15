2f4f9371618d5298cda72514804a85f2171dcedc
Colin Clark <colinbdclark@gmail.com>
2015-08-15 13:34:10 -0400

    Version 1.3.0.

--- dist/osc-chromeapp.js
+++ dist/osc-chromeapp.js
@@ -1,4 +1,4 @@
-/*! osc.js 1.2.1, Copyright 2015 Colin Clark | github.com/colinbdclark/osc.js */
+/*! osc.js 1.3.0, Copyright 2015 Colin Clark | github.com/colinbdclark/osc.js */
 
 /*
  * osc.js: An Open Sound Control library for JavaScript that works in both the browser and Node.js

19c6f042bc63a282ca2a43077372c7e4d0c2455f
Colin Clark <colinbdclark@gmail.com>
2016-02-01 16:09:04 -0600

    Updates built files.

--- dist/osc.js
+++ dist/osc.js
@@ -1,4 +1,4 @@
-/*! osc.js 2.0.2, Copyright 2015 Colin Clark | github.com/colinbdclark/osc.js */
+/*! osc.js 2.0.2, Copyright 2016 Colin Clark | github.com/colinbdclark/osc.js */
 
 /*
  * osc.js: An Open Sound Control library for JavaScript that works in both the browser and Node.js

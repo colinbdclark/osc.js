41e9969978c057c0b6675559fc2465ec53d49a4a
Colin Clark <colinbdclark@gmail.com>
2017-01-01 21:12:41 -0500

    Version 2.2.0

--- dist/osc-module.js
+++ dist/osc-module.js
@@ -1,4 +1,4 @@
-/*! osc.js 2.1.0, Copyright 2017 Colin Clark | github.com/colinbdclark/osc.js */
+/*! osc.js 2.2.0, Copyright 2017 Colin Clark | github.com/colinbdclark/osc.js */
 
 (function (root, factory) {
     if (typeof exports === "object") {

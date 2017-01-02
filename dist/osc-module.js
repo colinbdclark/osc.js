f0b595365ac307f60a25d86f884bc407401cee4b
Colin Clark <colinbdclark@gmail.com>
2017-01-01 19:36:01 -0500

    gh-75: Updates third party dependencies.

--- dist/osc-module.js
+++ dist/osc-module.js
@@ -1,4 +1,4 @@
-/*! osc.js 2.1.0, Copyright 2016 Colin Clark | github.com/colinbdclark/osc.js */
+/*! osc.js 2.1.0, Copyright 2017 Colin Clark | github.com/colinbdclark/osc.js */
 
 (function (root, factory) {
     if (typeof exports === "object") {

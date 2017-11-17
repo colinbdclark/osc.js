45e2af83070554c8c3706a019442e139c2d77464
Colin Clark <colinbdclark@gmail.com>
2017-11-16 19:11:39 -0500

    Improves gh-94 to bind open with "on" instead of "once. Also removes other now-unncessary callbacks that emit errors. Thanks to @sonnyp for the fix!

--- src/platforms/osc-node.js
+++ src/platforms/osc-node.js
@@ -90,7 +90,7 @@
             that.emit("error", err);
         });
 
-        this.serialPort.once("open", function () {
+        this.serialPort.on("open", function () {
             that.emit("open", that.serialPort);
         });
 
@@ -104,12 +104,8 @@
             that.emit("data", data, undefined);
         });
 
-        this.serialPort.on("close", function (err) {
-            if (err) {
-                that.emit("error", err);
-            } else {
-                that.emit("close");
-            }
+        this.serialPort.on("close", function () {
+            that.emit("close");
         });
 
         that.emit("ready");
@@ -122,11 +118,7 @@
         }
 
         var that = this;
-        this.serialPort.write(encoded, function (err) {
-            if (err) {
-                that.emit("error", err);
-            }
-        });
+        this.serialPort.write(encoded);
     };
 
     p.close = function () {

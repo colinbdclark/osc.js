d280635e1bf2679c6b6da69f7c65e2e62d00cccb
Colin Clark <colinbdclark@gmail.com>
2019-05-15 15:06:50 -0400

    Removes use of Buffer constructor now that Node 10 is LTS.

--- src/osc.js
+++ src/osc.js
@@ -194,13 +194,7 @@ var osc = osc || {};
     };
 
     osc.readString.withBuffer = function (charCodes) {
-        // new Buffer() is now deprecated,
-        // so we use Buffer.from if available
-        if (Buffer.from) {
-            return Buffer.from(charCodes).toString("utf-8");
-        } else {
-            return new Buffer(charCodes).toString("utf-8");
-        }
+        return Buffer.from(charCodes).toString("utf-8");
     };
 
     /**

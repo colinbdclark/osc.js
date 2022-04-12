d138efe118074264824cabf8fc8f866e798c8900
bobbyjenkins34 <73011712+bobbyjenkins34@users.noreply.github.com>
2022-04-12 16:35:21 -0400

    chore: update serialport to use NAPI
    This updates serial port to 10.4 where serialport now uses napi.
    
    Serialport has also switching from @serialport/bindings to @serialport/bindings-cpp which also uses napi and prebuildify

--- dist/osc.js
+++ dist/osc.js
@@ -1,4 +1,4 @@
-/*! osc.js 2.4.1, Copyright 2021 Colin Clark | github.com/colinbdclark/osc.js */
+/*! osc.js 2.4.2, Copyright 2022 Colin Clark | github.com/colinbdclark/osc.js */
 
 /*
  * osc.js: An Open Sound Control library for JavaScript that works in both the browser and Node.js
